ae7f519da31177df9d5559fdc6262bbaa367db23
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-10-10 10:42:17 +0530

    Refactor UART receiver module for clarity and correctness

--- UART/uart_receiver.v
+++ UART/uart_receiver.v
@@ -1,22 +1,21 @@
 module receiver(
-  input clk2,rst,baud_rclk,rx,
-  output reg done_r,error
+  input clk2, rst, baud_rclk, rx,
+  output reg done_r, error
 );
   
-  
-  reg [1:0]ps,ns;
-  reg [3:0]count;
-  reg [7:0]data;
+  reg [1:0] ps, ns;
+  reg [3:0] count;
+  reg [7:0] data;
   
   parameter idle = 2'b00, start = 2'b01, parity = 2'b10;
   
-  
-  always@(posedge clk2) begin
+  always @(posedge clk2) begin
     if(rst) begin
       ps <= idle;
       ns <= idle;
       count <= 4'd0;
       done_r <= 1'b0;
+      error <= 1'b0;
       data <= 8'd0;
     end
     else begin
@@ -24,16 +23,14 @@ module receiver(
     end
   end
   
-  always@(posedge baud_rclk) begin
+  always @(posedge baud_rclk) begin
     case(ps)
-      idle : begin 
-        if(!rx) begin
-          ns <= start;
-        end
+      idle: begin 
+        if(!rx) ns <= start;
         else ns <= idle;
       end
-      start : begin
-        if(count <= 8) begin
+      start: begin
+        if(count <= 7) begin
           data[count] <= rx;
           count <= count + 1'b1;
         end
@@ -41,20 +38,20 @@ module receiver(
           ns <= parity;
         end
       end
-      parity : begin
+      parity: begin
         if(((^data)^ rx) == 1'b0) begin 
           done_r <= 1'b1;
-          error <= 0;
-          ns <= idle;
-        end
-        else begin
           error <= 1'b1;
           ns <= idle;
         end
+        else begin
+          done_r <= 1'b1;
+          error <= 1'b0;
+          ns <= idle;
+        end
       end
-      default ns <= idle;
+      default: ns <= idle;
     endcase
   end
-  
-  
+
 endmodule
