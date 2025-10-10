9c82552dcfb5e3ce94d9cc237d31567d78a7ecc4
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-10-10 10:43:01 +0530

    Refactor UART testbench for clarity and timing
    Formatted code for better readability and adjusted clock period.

--- UART/uart_tb.v
+++ UART/uart_tb.v
@@ -1,22 +1,32 @@
 module tb();
   
-  reg clk1,clk2,rst;
-  reg [7:0]data;
+  reg clk1, clk2, rst;
+  reg [7:0] data;
   
-  wire baud_clk_R,baud_clk_T,tx,done_t,done_r,error;
-
+  wire baud_clk_R, baud_clk_T, tx, done_t, done_r, error;
   
   initial begin 
     clk1 = 1;
     clk2 = 1;
   end
   
-  always #10clk1 = ~clk1;
-  always #12.5clk2 = ~clk2;
+  always #10 clk1 = ~clk1;
+  always #5 clk2 = ~clk2;
   
-  top_module dut (.clk1(clk1),.clk2(clk2),.rst(rst),.baud_clk_T(baud_clk_T),.baud_clk_R(baud_clk_R),.data(data),.tx(tx),.done_t(done_t),.done_r(done_r),.error(error));
+  top_module dut(
+    .clk1(clk1),
+    .clk2(clk2),
+    .rst(rst),
+    .baud_clk_T(baud_clk_T),
+    .baud_clk_R(baud_clk_R),
+    .data(data),
+    .tx(tx),
+    .done_t(done_t),
+    .done_r(done_r),
+    .error(error)
+  );
   
-  initial begin 
+  initial begin
     $dumpfile("dump.vcd");
     $dumpvars();
   end
@@ -27,6 +37,8 @@ module tb();
     #10 data = 8'b10010101;
 
     #100000;
+    
     $finish;
   end
+  
 endmodule
